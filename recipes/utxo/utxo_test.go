c4e5ad85f67c406659385b7d08759e5e91e6af57
yqsy <yqsy021@126.com>
2018-08-26 14:35:52 +0800

    第一个测试用例完成

--- recipes/utxo/utxo_test.go
+++ recipes/utxo/utxo_test.go
@@ -16,33 +16,16 @@ func getRandomBit(len int) []byte {
 
 // a. WalletAddr-B 全部转出,那么这笔交易在存储池中就被删除
 func TestSimpleA(t *testing.T) {
-	publicKeyHashA := getRandomBit(20)
-	txIdHash1 := getRandomBit(32)
+	os.Remove("utxoDb.db")
 
-	txOutsA := []TxOut{TxOut{10, publicKeyHashA}}
 	utxoDb := NewUtxoDb("utxoDb.db")
 	defer func() { os.Remove("utxoDb.db") }()
 
-	// 交易1 手动放
-	if err := utxoDb.db.Update(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte(utxoBucket))
+	// 交易1: 创建公钥哈希A
+	publicKeyHashA, txIdHash1 := createTxOnlyOut(utxoDb, 10, t)
 
-		if err := b.Put(txIdHash1, EncodeTxOuts(txOutsA)); err != nil {
-			t.Fatal(err)
-		}
-		return nil
-	}); err != nil {
-		t.Fatal(err)
-	}
-
-	// 交易2: A => B
-	publicKeyHashB := getRandomBit(20)
-	txIdHash2 := getRandomBit(32)
-
-	txInsB := []TxIn{TxIn{txIdHash1, 0}}
-	txOutsB := []TxOut{TxOut{10, publicKeyHashB}}
-
-	utxoDb.update(txInsB, txOutsB, txIdHash2)
+	// 交易2: 创建公钥哈希B, A => B
+	publicKeyHashB, txIdHash2 := createTxWithIn(utxoDb, TxIn{txIdHash1, 0}, 10, nil)
 
 	// 1. A可花费输出为空 2. B可花费输出为10个
 	remainAmount, spendableOuts := utxoDb.findSpendableTxOutIdx(publicKeyHashA, 10)
@@ -65,11 +48,7 @@ func TestSimpleA(t *testing.T) {
 	}
 
 	// 1. A余额为空 2. B余额为10
-	if utxoDb.getBalance(publicKeyHashA) != 0 {
-		t.Fatal("err")
-	}
-
-	if utxoDb.getBalance(publicKeyHashB) != 10 {
+	if utxoDb.getBalance(publicKeyHashA) != 0  || utxoDb.getBalance(publicKeyHashB) != 10{
 		t.Fatal("err")
 	}
 
@@ -77,4 +56,71 @@ func TestSimpleA(t *testing.T) {
 	if utxoDb.countTransactions() != 1 {
 		t.Fatal("err")
 	}
+
+	// 交易3: 创建公钥哈希C, B => C 全部转
+	publicKeyHashC, txIdHash3 := createTxWithIn(utxoDb, TxIn{txIdHash2, 0}, 10, nil)
+
+	// 1. B可花费输出为空 2. C可花费输出为10个
+	remainAmount, spendableOuts = utxoDb.findSpendableTxOutIdx(publicKeyHashB, 10)
+	if remainAmount != 0 {
+		t.Fatal("err")
+	}
+	spendableOutsEqual = make(map[string]int)
+	if !reflect.DeepEqual(spendableOuts, spendableOutsEqual) {
+		t.Fatal("err")
+	}
+
+	remainAmount, spendableOuts = utxoDb.findSpendableTxOutIdx(publicKeyHashC, 10)
+	if remainAmount != 10 {
+		t.Fatal("err")
+	}
+	spendableOutsEqual = make(map[string]int)
+	spendableOutsEqual[string(txIdHash3)] = 0
+	if !reflect.DeepEqual(spendableOuts, spendableOutsEqual) {
+		t.Fatal("err")
+	}
+
+	// 1. B余额为空 2. C余额为10
+	if utxoDb.getBalance(publicKeyHashB) != 0  || utxoDb.getBalance(publicKeyHashC) != 10{
+		t.Fatal("err")
+	}
+
+	// 交易数量为 1
+	if utxoDb.countTransactions() != 1 {
+		t.Fatal("err")
+	}
+}
+
+// 创造一个输出 (依赖in)
+func createTxWithIn(utxoDb *UtxoDb, txIn TxIn, transferAmount int, backOut *TxOut /*返还的*/) (publicKeyHash []byte, txId []byte) {
+	txId = getRandomBit(32)
+	publicKeyHash = getRandomBit(20)
+	txOuts := []TxOut{TxOut{transferAmount, publicKeyHash}}
+
+	if backOut != nil {
+		txOuts = append(txOuts, *backOut)
+	}
+
+	utxoDb.update([]TxIn{txIn}, txOuts, txId)
+	return publicKeyHash, txId
+}
+
+// 创造一个输出 (仅有输出)
+func createTxOnlyOut(utxoDb *UtxoDb, amount int, t *testing.T) (publicKeyHash []byte, txId []byte) {
+	txId = getRandomBit(32)
+	publicKeyHash = getRandomBit(20)
+
+	txOuts := []TxOut{TxOut{amount, publicKeyHash}}
+
+	if err := utxoDb.db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(utxoBucket))
+
+		if err := b.Put(txId, EncodeTxOuts(txOuts)); err != nil {
+			t.Fatal(err)
+		}
+		return nil
+	}); err != nil {
+		t.Fatal(err)
+	}
+	return publicKeyHash, txId
 }
