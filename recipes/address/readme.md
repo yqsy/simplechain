5afb95d9fb962e8db5d5e06d970aa7357605d25c
yqsy <yqsy021@126.com>
2018-08-28 21:27:49 +0800

    update

--- recipes/address/readme.md
+++ recipes/address/readme.md
@@ -1,8 +1,8 @@
 <!-- TOC -->
 
 - [1. 相关知识](#1-相关知识)
-- [2. 私钥/公钥/公钥哈希/钱包地址在bitcoin中](#2-私钥公钥公钥哈希钱包地址在bitcoin中)
-- [3. 私钥/公钥/公钥哈希/钱包地址](#3-私钥公钥公钥哈希钱包地址)
+- [2. 私钥/公钥/公钥哈希/钱包地址](#2-私钥公钥公钥哈希钱包地址)
+- [3. 私钥/公钥/公钥哈希/钱包地址在bitcoin中](#3-私钥公钥公钥哈希钱包地址在bitcoin中)
 - [4. sign/verify](#4-signverify)
 - [5. 实现列表](#5-实现列表)
 
@@ -11,28 +11,17 @@
 <a id="markdown-1-相关知识" name="1-相关知识"></a>
 # 1. 相关知识
 
+* https://github.com/pointbiz/bitaddress.org (地址生成源码)
+* https://www.bitaddress.org/ (比特币地址生成)
 * http://book.8btc.com/books/6/masterbitcoin2cn/_book/ch04.html (精通比特币-第四章 秘钥和地址)
+* https://en.bitcoin.it/wiki/List_of_address_prefixes (prefix)
+* https://en.bitcoin.it/wiki/Wallet_import_format (wallet import format)
 * http://www.youdzone.com/signature.html (签名+认证)
 * https://blog.csdn.net/jeason29/article/details/51576659 (图片来源)
 
 
-<a id="markdown-2-私钥公钥公钥哈希钱包地址在bitcoin中" name="2-私钥公钥公钥哈希钱包地址在bitcoin中"></a>
-# 2. 私钥/公钥/公钥哈希/钱包地址在bitcoin中
-
-
-私钥的存储格式:
-![](./pic/privatekeysaveway.png)
-
-公钥的存储格式:
-* 未压缩格式: 04作为前缀,payload 128 hex, 一共130hex
-* 压缩格式: 02或03作为前缀,payload 64 hex, 一共66hex
-
-所有的钱包版本  
-![](./pic/all_base58_version.png)
-
-
-<a id="markdown-3-私钥公钥公钥哈希钱包地址" name="3-私钥公钥公钥哈希钱包地址"></a>
-# 3. 私钥/公钥/公钥哈希/钱包地址
+<a id="markdown-2-私钥公钥公钥哈希钱包地址" name="2-私钥公钥公钥哈希钱包地址"></a>
+# 2. 私钥/公钥/公钥哈希/钱包地址
 
 类型|方式|hex长度|byte长度
 -|-|-|-
@@ -54,6 +43,21 @@ txId|SHA256|64|32
 ![](./pic/address-generation-extra.png)
 
 
+<a id="markdown-3-私钥公钥公钥哈希钱包地址在bitcoin中" name="3-私钥公钥公钥哈希钱包地址在bitcoin中"></a>
+# 3. 私钥/公钥/公钥哈希/钱包地址在bitcoin中
+
+
+私钥的存储格式:
+![](./pic/privatekeysaveway.png)
+
+公钥的存储格式:
+* 未压缩格式: 04作为前缀,payload 128 hex, 一共130hex
+* 压缩格式: 02或03作为前缀,payload 64 hex, 一共66hex
+
+base58-encode addr  
+![](./pic/all_base58_version.png)
+
+
 
 <a id="markdown-4-signverify" name="4-signverify"></a>
 # 4. sign/verify
@@ -71,9 +75,9 @@ txId|SHA256|64|32
 一笔转账:    
 ![](./pic/transfer_confirm.png)
 
-
 <a id="markdown-5-实现列表" name="5-实现列表"></a>
 # 5. 实现列表
+
 * RANDOM 生成私钥
 * SECP256K1 生成对应的公钥
 * SHA256 + RIPEMD160 生成公钥哈希
--- recipes/address/wallet.go
+++ recipes/address/wallet.go
@@ -17,12 +17,11 @@ const (
 )
 
 var (
-	Version = []byte{0}
+	P2PKHVersion = []byte{0}
+	WIFVersion   = []byte{0x80}
 )
 
-// 生成一对公私钥
-// Elliptic Curve Digital Signature Algorithm
-// https://en.wikipedia.org/wiki/Elliptic_Curve_Digital_Signature_Algorithm
+// 生成一对公私钥 // Elliptic Curve Digital Signature Algorithm // https://en.wikipedia.org/wiki/Elliptic_Curve_Digital_Signature_Algorithm
 func generateKeyPair() (*ecdsa.PrivateKey, *ecdsa.PublicKey) {
 	p256 := elliptic.P256()
 	if privateKey, err := ecdsa.GenerateKey(p256, rand.Reader); err != nil {
@@ -54,20 +53,25 @@ func generatePublicKeyHash(publicKey *ecdsa.PublicKey) []byte {
 	return ripemd160Sum
 }
 
-// 生成公钥哈希的checksum
-// 1.版本 2.公钥哈希
-func generatePublicKeyHashCheckSum(version []byte, publicKeyHash []byte) []byte {
-	payload := append(version, publicKeyHash...)
-	firstSha256Sum := sha256.Sum256(payload)
+// 生成checksum // 1.版本 2.payload
+func double256Sum(version []byte, payload []byte) []byte {
+	versionPayload := append(version, payload...)
+	firstSha256Sum := sha256.Sum256(versionPayload)
 	secondSha256Sum := sha256.Sum256(firstSha256Sum[:])
 	return secondSha256Sum[:]
 }
 
-// 生成钱包地址
-// 1.版本 2.公钥哈希 3.checksum截断值
-func generateWalletAddress(version []byte, publicKeyHash []byte, checksumCut []byte) []byte {
-	payload := append(append(version, publicKeyHash...), checksumCut...)
-	return base58.Base58Encode(payload)
+// 生成地址 // 1.版本 2.payload 3.checksum截断值
+func base58PayloadWithVersionAndChecksumCut(version []byte, payload []byte, checksumCut []byte) []byte {
+	versionPayloadChecksumCut := append(append(version, payload...), checksumCut...)
+	return base58.Base58Encode(versionPayloadChecksumCut)
+}
+
+// 1. 生成checksum 2. base58 (version,payload,checkSumCut)
+func convertVersionAndPayloadToAddress(version []byte, payload []byte) []byte{
+	checkSum := double256Sum(version, payload)
+	checkSumCut := checkSum[:CheckSumLen]
+	return base58PayloadWithVersionAndChecksumCut(version, payload, checkSumCut)
 }
 
 // 钱包地址提取公钥哈希
@@ -106,43 +110,65 @@ func convertSignatureTors(signature []byte) (*big.Int, *big.Int) {
 	return r, s
 }
 
-// 这里为了把所有的成员放置在一起
-// 实际上wallet只需要 1. privateKey 2. publicKey
+// 这里为了把所有的成员放置在一起 // 实际上wallet只需要 1. privateKey 2. publicKey
 type Wallet struct {
-	privateKey               *ecdsa.PrivateKey // 私钥
-	publicKey                *ecdsa.PublicKey  // 公钥
-	privateKeyBytes          []byte            // 私钥二进制数组
-	publicKeyBytes           []byte            // 公钥二进制数组
-	privateKeyEncoded        string            // 私钥x509 encode
-	publicKeyEncoded         string            // 公钥x509 encode
-	publicKeyHash            []byte            // 公钥hash
-	publicKeyHashCheckSum    []byte            // [版本+公钥哈希]校验值
-	publicKeyHashCheckSumCut []byte            // [版本+公钥哈希]校验值前4字节
-	walletAddress            []byte            // 钱包地址
+	// --- 我自己实现的部分
+	privateKey        *ecdsa.PrivateKey // 私钥
+	publicKey         *ecdsa.PublicKey  // 公钥
+	privateKeyBytes   []byte            // 私钥二进制数组
+	publicKeyBytes    []byte            // 公钥二进制数组
+	privateKeyEncoded string            // 私钥x509 encode
+	publicKeyEncoded  string            // 公钥x509 encode
+	publicKeyHash     []byte            // 公钥hash
+	walletAddress     []byte            // 钱包地址
+
+	// --- 比特币
+	btcPrivateKeyBytes    []byte // 私钥
+	btcPrivateKeyWif      []byte // 私钥的Wallet import format  (5 前缀)
+	btcPublicKeyBytes     []byte // 公钥 (04 前缀)
+	btcWalletAddressP2PKH []byte // P2PKH钱包地址 (1 前缀)
+	btcWalletAddressP2SH  []byte // P2SH钱包地址 (3 前缀)
+
+	// --- 压缩
+	btcPrivateKeyBytesCompressed    []byte // 私钥 (01 末尾)
+	btcPrivateKeyWifCompressed      []byte // 私钥的Wallet import format (K,L前缀)
+	btcPublicKeyBytesCompressed     []byte // 公钥 (02 03 前缀)
+	btcWalletAddressP2PKHCompressed []byte // P2PKH钱包地址 (1 前缀)
+	btcWalletAddressP2SHCompressed  []byte // P2SH钱包地址 (3 前缀)
 }
 
 func NewWallet() *Wallet {
 	wallet := &Wallet{}
+
+	// --- 我自己实现的部分
 	wallet.privateKey, wallet.publicKey = generateKeyPair()
 	wallet.privateKeyEncoded, wallet.publicKeyEncoded = encode(wallet.privateKey, wallet.publicKey)
 	wallet.privateKeyBytes = getPrivateKeyBytes(wallet.privateKey)
 	wallet.publicKeyBytes = getPublicKeyBytes(wallet.publicKey)
 	wallet.publicKeyHash = generatePublicKeyHash(wallet.publicKey)
-	wallet.publicKeyHashCheckSum = generatePublicKeyHashCheckSum(Version, wallet.publicKeyHash)
-	wallet.publicKeyHashCheckSumCut = wallet.publicKeyHashCheckSum[:CheckSumLen]
-	wallet.walletAddress = generateWalletAddress(Version, wallet.publicKeyHash, wallet.publicKeyHashCheckSumCut)
+	wallet.walletAddress = convertVersionAndPayloadToAddress(P2PKHVersion, wallet.publicKeyHash)
+
+	// --- 比特币
+	wallet.btcPrivateKeyBytes = wallet.privateKeyBytes
+	wallet.btcPrivateKeyWif = convertVersionAndPayloadToAddress(WIFVersion, wallet.btcPrivateKeyBytes)
+	wallet.btcPublicKeyBytes = append([]byte{0, 4}, wallet.publicKeyBytes...)
+	wallet.btcWalletAddressP2PKH = wallet.walletAddress
+
+
 	return wallet
 }
 
 func (wallet *Wallet) String() string {
 	result := ""
+	result += fmt.Sprintf("in my Realization:\n")
 	result += fmt.Sprintf("%v\n\n", wallet.privateKeyEncoded)
 	result += fmt.Sprintf("%v\n\n", wallet.publicKeyEncoded)
 	result += fmt.Sprintf("privateKeyBytes: %x\n", wallet.privateKeyBytes)
 	result += fmt.Sprintf("publicKeyBytes: %x\n", wallet.publicKeyBytes)
-	result += fmt.Sprintf("version: %v\n", Version)
 	result += fmt.Sprintf("publickeyHash: %x\n", wallet.publicKeyHash)
-	result += fmt.Sprintf("publicKeyHashCheckSumCut: %x\n", wallet.publicKeyHashCheckSumCut)
 	result += fmt.Sprintf("walletAddress: %s\n", wallet.walletAddress)
+
+	result += fmt.Sprintf("in <bitcoin>:\n")
+
 	return result
 }
